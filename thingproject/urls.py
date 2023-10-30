f131209c122220dac400f081b32728ce7bbb2eee
Yohann Pirbay <pirbay.yohann@gmail.com>
2023-10-30 20:36:09 +0000

    Include added

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from things.views import home
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include("things.urls"), name='home'),
 ]
